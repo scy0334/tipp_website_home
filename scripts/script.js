1627402f7a49e52b459eb328b86a2a7fb5a57c14
nero <>
2020-10-29 19:46:59 +0900

    s

--- scripts/script.js
+++ scripts/script.js
@@ -163,7 +163,29 @@ $(document).ready(function () {
         $('.page1,.footer-desc').hide()
     }
 
+    $('vid')
+
+
+    var isIOS = true
+    //var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+    if (isIOS) {
+        var canvasVideo = new CanvasVideoPlayer({
+            videoSelector: '.video',
+            canvasSelector: '.canvas',
+            timelineSelector: false,
+            autoplay: true,
+            makeLoop: true,
+            pauseOnClick: false,
+            audio: false
+        });
+    }else {
+        // Use HTML5 video
+        $('.canvas').css('display','none')
+        //document.querySelectorAll('.canvas')[0].style.display = 'none';
+    }
 })
+
 
 setTimeout(() => {
     $(".load_modal").css('display','none');
